802f53778ebb78d0ea0c6a7158c88278c4e489f1
salim abdullahi <salimfaris15@gmail.com>
2018-08-27 10:45:25 +0300

    created global var

--- js/scripts.js
+++ js/scripts.js
@@ -20,29 +20,29 @@ var score = 0;//Saves total score
 var scores = []//Saves cumulative score for individual questions
 
 //Store all questions in an array
-var questions=[
-  {
-    question: "1.What is javascript?",
-    choices: ["scripting language","language", "none"],
-    answer: "scripting language",
-  },
-  {
-  question: "2.is javascript similar to java?",
-  choices: ["No","yes", "no idea"],
-  answer: "No",
-  },
-  {
-  question: "3.can javascript be used to build applications?",
-  choices: ["yes","it depends","No"],
-  answer: "yes",
-  },
+// var questions=[
+//   {
+//     question: "1.What is javascript?",
+//     choices: ["scripting language","language", "none"],
+//     answer: "scripting language",
+//   },
+//   {
+//   question: "2.is javascript similar to java?",
+//   choices: ["No","yes", "no idea"],
+//   answer: "No",
+//   },
+//   {
+//   question: "3.can javascript be used to build applications?",
+//   choices: ["yes","it depends","No"],
+//   answer: "yes",
+//   },
 ]
-
-$(document).ready(function () {
-  $("#question1").text(questions[0].question);
-  $("#question2").text(questions[1].question);
-  $("#question3").text(questions[2].question);
-});
+//
+// $(document).ready(function () {
+//   $("#question1").text(questions[0].question);
+//   $("#question2").text(questions[1].question);
+//   $("#question3").text(questions[2].question);
+// });
 // $(document).ready(function() {
 //         $("button#hello").click(function() {
 //           $("ul#user").prepend("<li>Hello!</li>");
